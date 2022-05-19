1520c5eb79dbd2cdade76923097b4dcae51cadbe
Jenny Chau <jnechau@gmail.com>
2022-05-18 22:00:35 -0400

    first commit

---
+++ ArtBank.js
@@ -0,0 +1,178 @@
+artBank = [
+    {
+        id: 0,
+        artist: "Leonardo da Vinci",
+        title: "The Last Supper",
+        year: 1498
+    },
+
+    {
+        id: 1,
+        artist: "Vincent Van Gogh",
+        title: "Irises",
+        year: 1889
+    },
+
+    {
+        id: 2,
+        artist: "Vincent Van Gogh",
+        title: "Wheat Field with Cypresses",
+        year: 1889
+    },
+
+    {
+        id: 3,
+        artist: "Vincent Van Gogh",
+        title: "The Starry Night",
+        year: 1889
+    },
+
+    {
+        id: 4,
+        artist: "Leonardo da Vinci",
+        title: "Portrait of Mona Lisa del Giocondo",
+        year: 1506
+    },
+    
+    {
+        id: 5,
+        artist: "Johannes Vermeer",
+        title: "Girl with a Pearl Earring",
+        year: 1665
+    },
+    
+    {
+        id: 6,
+        artist: "Gustav Klimt",
+        title: "The Kiss",
+        year: 1908
+    },
+
+    {
+        id: 7,
+        artist: "Georges Seurat",
+        title: "A Sunday on La Grande Jatte",
+        year: 1884
+    },
+
+    {
+        id: 8,
+        artist: "Caspar David Friedrich",
+        title: "New Moon above the Riesengebirge Mountains",
+        year: 1810,
+        yearAlt: "or between 1828 and 1835"
+    },
+
+    {
+        id: 9,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 10,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 11,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 12,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 13,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 14,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 15,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 16,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 17,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 18,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 19,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 20,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 21,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 22,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 23,
+        artist: "",
+        title: "",
+        year: 0
+    },
+
+    {
+        id: 24,
+        artist: "",
+        title: "",
+        year: 0
+    },
+    
+]
